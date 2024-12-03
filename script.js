08a4ac10bd1c91f9ab64b4c66585207da5ed9bd2
hamza rehman <mohdhamzarehman@gmail.com>
2024-12-03 16:22:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const expenseForm =  document.getElementById("expense-form");
+  const expenseName =  document.getElementById("expense-name");
+  const expenseAmount =  document.getElementById("expense-amount");
+  const expenseList =  document.getElementById("expense-list");
+  const totalAmountDisplay = document.getElementById("total-amount")
+  let expenses = []
+  let totalAmount = calculateTotal()
+  expenseForm.addEventListener('submit',(e)=>{
+    e.
+  })
+  function calculateTotal(){
+
+  }
+});
